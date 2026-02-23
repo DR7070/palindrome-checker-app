51c26d0dc648aed36f2135953513644cda6cb35d
DR7070 <dakshraheja07@gmail.com>
2026-02-23 15:03:33 +0530

    Update UseCase3PalindromeCheckerApp.java

--- UseCase3PalindromeCheckerApp.java
+++ UseCase3PalindromeCheckerApp.java
@@ -2,18 +2,17 @@ public class UseCase3PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // Original String
+
         String original = "level";
 
-        // Reversed String
+    
         String reversed = "";
 
-        // Reverse using for loop
+  
         for (int i = original.length() - 1; i >= 0; i--) {
             reversed = reversed + original.charAt(i);
         }
 
-        // Compare using equals() method
         if (original.equals(reversed)) {
             System.out.println("The string \"" + original + "\" is a Palindrome.");
         } else {
