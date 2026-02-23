f03381576129e7c36dd913aa55679460668c05ba
DR7070 <dakshraheja07@gmail.com>
2026-02-23 15:07:41 +0530

    Update UseCase7PalindromeCheckerApp.java

--- UseCase7PalindromeCheckerApp.java
+++ UseCase7PalindromeCheckerApp.java
@@ -5,24 +5,23 @@ public class UseCase7PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // Original String
+       
         String word = "radar";
 
-        // Create Deque
+     
         Deque<Character> deque = new ArrayDeque<>();
 
-        // Insert characters into deque
+
         for (int i = 0; i < word.length(); i++) {
             deque.addLast(word.charAt(i));
         }
 
         boolean isPalindrome = true;
 
-        // Compare front and rear elements
         while (deque.size() > 1) {
 
-            char front = deque.removeFirst(); // Remove from front
-            char rear = deque.removeLast();   // Remove from rear
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();   
 
             if (front != rear) {
                 isPalindrome = false;
@@ -30,7 +29,6 @@ public class UseCase7PalindromeCheckerApp {
             }
         }
 
-        // Print Result
         if (isPalindrome) {
             System.out.println("The string \"" + word + "\" is a Palindrome.");
         } else {
